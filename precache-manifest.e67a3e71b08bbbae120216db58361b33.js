75202815ffead0d063375558678c0214688ec438
Jessiecodes <Jessak1202@gmail.com>
2020-09-29 10:17:00 -0400

    Updates

---
+++ precache-manifest.e67a3e71b08bbbae120216db58361b33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e689d74fbab6ea8da579d805c605242",
+    "url": "/reacttodo/index.html"
+  },
+  {
+    "revision": "e67ec2149a20ce902d6d",
+    "url": "/reacttodo/static/css/main.b204e063.chunk.css"
+  },
+  {
+    "revision": "ae952f9a0d71725f3f64",
+    "url": "/reacttodo/static/js/2.e109ed59.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reacttodo/static/js/2.e109ed59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e67ec2149a20ce902d6d",
+    "url": "/reacttodo/static/js/main.5d35d435.chunk.js"
+  },
+  {
+    "revision": "a7aeb6b4c92a6a368fbd",
+    "url": "/reacttodo/static/js/runtime-main.8a5f4ff5.js"
+  }
+]);
